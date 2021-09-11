4a31d762768ac8a110248836aae0185c842bef6d
Vint_Razor <vint-razor@yandex.ru>
2021-09-11 17:59:42 +0500

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,48 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        if (firstRowIndex(board) >= 0 && checkColumn(board, firstRowIndex(board))) {
+            return true;
+        } else if (firstColumnIndex(board) >= 0 && checkRow(board, firstColumnIndex(board))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int firstColumnIndex(int[][] array) {
+        for (int i = 0; i < array[0].length; i++) {
+            if (array[0][i] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int firstRowIndex(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i][0] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean checkColumn(int[][] array, int index) {
+        for (int num : array[index]) {
+            if (num != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkRow(int[][] array, int index) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i][index] != 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-@Ignore
+//@Ignore
 public class WinTest {
     @Test
     public void whenVerticalWin() {
@@ -16,6 +16,18 @@ public class WinTest {
                 {0, 0, 1, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.check(board), is(true));
+    }
+
+    @Test
+    public void whenFirstVerticalWin() {
+        int[][] board = {
+                {1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0},
         };
         assertThat(Win.check(board), is(true));
     }
@@ -54,5 +66,105 @@ public class WinTest {
                 {1, 1, 1, 1, 0},
         };
         assertThat(Win.check(board), is(false));
+    }
+
+    @Test
+    public void when0RowIndex2Then2() {
+        int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {1, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.firstColumnIndex(board), is(2));
+    }
+
+    @Test
+    public void whenNot0RowThenMinus1() {
+        int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {1, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.firstColumnIndex(board), is(-1));
+    }
+
+    @Test
+    public void when0ColumnIndex3Then3() {
+        int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {1, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.firstRowIndex(board), is(3));
+    }
+
+    @Test
+    public void whenNot0ColumnThenMinus1() {
+        int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.firstRowIndex(board), is(-1));
+    }
+
+    @Test
+    public void whenNotFullThenFalse() {
+        int index = 2;
+        int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1},
+                {0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.checkColumn(board, index), is(false));
+    }
+
+    @Test
+    public void when2FullThenFalse() {
+        int index = 2;
+        int[][] board = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+        };
+        assertThat(Win.checkColumn(board, index), is(true));
+    }
+
+    @Test
+    public void whenNotFullRowThenFalse() {
+        int index = 2;
+        int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.checkRow(board, index), is(false));
+    }
+
+    @Test
+    public void whenFull2RowThenTrue() {
+        int index = 2;
+        int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        assertThat(Win.checkRow(board, index), is(true));
     }
 }
