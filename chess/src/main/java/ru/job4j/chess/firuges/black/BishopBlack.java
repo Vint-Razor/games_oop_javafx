cd573fa211173c08846e3ae155baaa285fef325b
Рустам <vint-razor@yandex.ru>
2021-10-25 17:42:43 +0500

    написал метод isDiagonal покрыл тестами isDiagonal

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -24,7 +24,15 @@ public class BishopBlack implements Figure {
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        return false;
+        int x1 = source.getX();
+        int x2 = dest.getX();
+        int y1 = source.getY();
+        int y2 = dest.getY();
+        if (x1 < x2 && y1 > y2 || x1 > x2 && y1 < y2 ) {
+            return x1 + y1 == x2 + y2;
+        } else {
+            return x1 - y1 == x2 - y2;
+        }
     }
 
     @Override
--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -8,13 +8,36 @@ import ru.job4j.chess.firuges.Figure;
 
 public class BishopBlackTest {
 
-    @Ignore
     @Test
     public void whenC2ToG5ThenTrue() {
-        BishopBlack right = new BishopBlack(Cell.C2);
+        BishopBlack right = new BishopBlack(Cell.C1);
         Cell dest = Cell.G5;
         boolean expected = right.isDiagonal(right.position(), dest);
         assertTrue(expected);
+    }
+
+    @Test
+    public void whenG5ToC2ThenTrue() {
+        BishopBlack right = new BishopBlack(Cell.G5);
+        Cell dest = Cell.C1;
+        boolean expected = right.isDiagonal(right.position(), dest);
+        assertTrue(expected);
+    }
+
+    @Test
+    public void whenC5ToF2ThenTrue() {
+        BishopBlack right = new BishopBlack(Cell.C5);
+        Cell dest = Cell.F2;
+        boolean expected = right.isDiagonal(right.position(), dest);
+        assertTrue(expected);
+    }
+
+    @Test
+    public void whenC2ToG7ThenFalse() {
+        BishopBlack right = new BishopBlack(Cell.C2);
+        Cell dest = Cell.G7;
+        boolean expected = right.isDiagonal(right.position(), dest);
+        assertFalse(expected);
     }
 
     @Test
